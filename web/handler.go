61b324f6d5a8dc1a23b3b7e082bdcb31dc9e082a
Adam Eury <adam@coherentpath.com>
2022-02-20 14:44:16 -0500

    Add handlers for creating and deleting threads.

--- web/handler.go
+++ web/handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aleury/goreddit"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/google/uuid"
 )
 
 type Handler struct {
@@ -25,6 +26,9 @@ func NewHandler(store goreddit.Store) *Handler {
 
 	h.Route("/threads", func(r chi.Router) {
 		r.Get("/", h.ThreadsList())
+		r.Get("/new", h.ThreadsNew())
+		r.Post("/", h.ThreadsCreate())
+		r.Post("/{id}/delete", h.ThreadsDelete())
 	})
 
 	return h
@@ -36,8 +40,14 @@ const threadsListHTML = `
 {{range .Threads}}
 	<dt><strong>{{.Title}}</strong></dt>
 	<dd>{{.Description}}</dd>
+	<dd>
+		<form action="/threads/{{.ID}}/delete" method="POST">
+			<button type="submit">Delete</button>
+		</form>
+	</dd>
 {{end}}
 </dl>
+<a href="/threads/new">Create thread</a>
 `
 
 func (h *Handler) ThreadsList() http.HandlerFunc {
@@ -46,13 +56,76 @@ func (h *Handler) ThreadsList() http.HandlerFunc {
 	}
 
 	tmpl := template.Must(template.New("").Parse(threadsListHTML))
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(rw http.ResponseWriter, r *http.Request) {
 		tt, err := h.store.Threads()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		tmpl.Execute(w, data{Threads: tt})
+		tmpl.Execute(rw, data{Threads: tt})
+	}
+}
+
+const threadCreateHTML = `
+<h1>New thread</h1>
+<form action="/threads" method="POST">
+	<table>
+		<tr>
+			<td>Title</td>
+			<td><input type="text" name="title" /></td>
+		</tr>
+		<tr>
+			<td>Description</td>
+			<td><input type="text" name="description" /></td>
+		</tr>
+	</table>
+	<button type="submit">Create thread</button>
+</form>
+`
+
+func (h *Handler) ThreadsNew() http.HandlerFunc {
+	tmpl := template.Must(template.New("").Parse(threadCreateHTML))
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, nil)
+	}
+}
+
+func (h *Handler) ThreadsCreate() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		title := r.FormValue("title")
+		description := r.FormValue("description")
+
+		err := h.store.CreateThread(&goreddit.Thread{
+			ID:          uuid.New(),
+			Title:       title,
+			Description: description,
+		})
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(rw, r, "/threads", http.StatusFound)
+	}
+}
+
+func (h *Handler) ThreadsDelete() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = h.store.DeleteThread(id)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(rw, r, "/threads", http.StatusFound)
 	}
 }
